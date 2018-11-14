69a74f7399fa848251d1cb5a262fe38432571ce1
marcbrathwaite <marcbrathwaite@gmail.com>
2018-11-14 18:20:27 -0500

    Completed adding and removing entries from app

--- js/script.js
+++ js/script.js
@@ -1,16 +1,46 @@
-$(function () {
+//Module for the User interface
+const uiModule = (function () {
 
-    const displayMod = (function() {
+    const totalObjs = {
+        allObj: {},
+        expenseObj: {},
+        incomeObj: {}
+    };
+    // const allObj = {};
+    // const expenseObj = {};
+    // const incomeObj = {};
+    let id = 0;
 
-        //If negative return -$num else $num
-        function negativeCheck(num) {
-            // console.log(typeof num);
-            return num < 0 ? `-$${Math.abs(num).toFixed(2)}` : `$${num.toFixed(2)}`;
-        }
-        return {
-            //Function to display user input
-            displayEntry: function(date, transaction, category, description, amount ) {
-                const allHtml = `<ul class="output-disp-row">
+    //If negative return -$num else $num
+    function negativeCheck(num) {
+        return num < 0 ? `-$${Math.abs(num).toFixed(2)}` : `$${num.toFixed(2)}`;
+    }
+    return {
+        //Function to display user input
+        displayEntry: function (date, transaction, category, description, amount) {
+
+            const htmlID = `${transaction.toLowerCase()}-${id}`;
+
+            totalObjs["allObj"][htmlID] = {
+                date: date,
+                transaction: transaction,
+                category: category,
+                description: description,
+                amount: amount
+            };
+
+            // console.log(totalObjs["allObj"]);
+
+            totalObjs[`${transaction.toLowerCase()}Obj`][htmlID] = {
+                date: date,
+                category: category,
+                description: description,
+                amount: amount
+            }
+
+
+
+            const allHtml = `<ul class="output-disp-row" id="${htmlID}-list">
                             <li class="output-disp-col-med-all">
                                 ${date}
                             </li>
@@ -27,11 +57,11 @@ $(function () {
                                 $${parseFloat(amount).toFixed(2)}
                             </li>
                             <li class="output-disp-col-small-all">
-                                <i class="fas fa-times-circle"></i>
+                                <i class="fas fa-times-circle" id="${htmlID}"></i>
                             </li>
                         </ul>`;
-                
-                        const specHtml = `<ul class="output-disp-row">
+
+            const specHtml = `<ul class="output-disp-row" id="${htmlID}-list">
                             <li class="output-disp-col-med">
                                 ${date}
                             </li>
@@ -45,146 +75,206 @@ $(function () {
                                 $${parseFloat(amount).toFixed(2)}
                             </li>
                             <li class="output-disp-col-small">
-                                <i class="fas fa-times-circle"></i>
+                                <i class="fas fa-times-circle" id="${htmlID}"></i>
                             </li>
                         </ul>`;
 
-                        $('#all-tab').append(allHtml);
-                        $(`#${transaction.toLowerCase()}-tab`).append(specHtml);
-            },
-            //Function to display Expence/Income and Balance on the Dom
-            displayValues: function(transaction,expense, income, balance) {
-                if (transaction === 'Expense') {
-                    $('#expense-total').text(negativeCheck(expense));
-                } else {
-                    $('#income-total').text(negativeCheck(income));
-                }
-
-                $('#balance-total').text(negativeCheck(balance));
-
-            }
-        }
-    })();
-
-    const operationsMod = (function() {
-
-        // let income = 0;
-        // let expenses = 0;
-        // let balance = 0;
-
-        const values = {
-            income: 0,
-            expense: 0,
-            balance: 0
-        }
-
-
-        return {
-            //Update Income/Expenses or Balance
-            updateValues: function(transaction, amount) {
-                values[`${transaction.toLowerCase()}`] += parseFloat(amount);
-                values['balance'] = values['income'] - values['expense'];
-            },
-            getIncome: function() {
-                return values['income'];
-            },
-            getExpense: function() {
-                return values['expense'];
-            },
-            getBalance: function() {
-                return values['balance'];
+            $('#all-tab').append(allHtml);
+            $(`#${transaction.toLowerCase()}-tab`).append(specHtml);
+            id++; //increment ID
+        },
+        //Function to display Expence/Income and Balance on the Dom
+        displayValues: function (transaction, expense, income, balance) {
+            // if (transaction === 'Expense') {
+            if (/expense/i.test(transaction)) { 
+                $('#expense-total').text(negativeCheck(expense));
+            } else {
+                $('#income-total').text(negativeCheck(income));
             }
 
+            $('#balance-total').text(negativeCheck(balance));
 
+        },
+        //Check whether enter amount is a valid dollar amount
+        isValidAmount: function(amount) {
+            amount = amount.replace(/^\$/,"");
+            return $.isNumeric(amount) ? amount : false;
+        },
+        //Remove entry from the All table
+        removeFromTables: function(id) {
+            $(`#all-tab #${id}-list`).remove();
+            delete totalObjs["allObj"][id];
+            if(/expense/.test(id)) {
+                $(`#expense-tab #${id}-list`).remove();
+                delete totalObjs["expenseObj"][id];
+            } else {
+                $(`#income-tab #${id}-list`).remove();
+                delete totalObjs["incomeObj"][id];
+            }
+        }, 
+        getAllObj: function() {
+            return totalObjs["allObj"];
+        },
+        getIncomeObj: function() {
+            return totalObjs["incomeObj"];
+        },
+        getExpenseObj: function() {
+            return totalObjs["expenseObj"];
+        }
+    }
+})();
+
+//Module for handling calculations
+const opsModule = (function () {
+
+    // let income = 0;
+    // let expenses = 0;
+    // let balance = 0;
+
+    const values = {
+        income: 0,
+        expense: 0,
+        balance: 0
+    }
+
+    const updateBalance = function(){
+        values['balance'] = values['income'] - values['expense'];
+    };
+
+
+    return {
+        //Update Income/Expenses or Balance after input
+        addValues: function (transaction, amount) {
+            values[`${transaction.toLowerCase()}`] += parseFloat(amount);
+            // values['balance'] = values['income'] - values['expense'];
+            updateBalance();
+        },
+        subtractValues: function(id, allObj) {
+            if (/expense/.test(id)) {
+                values["expense"] -= allObj[id].amount;
+            } else {
+                values["income"] -= allObj[id].amount;
+            }
+            updateBalance();
+        },
+        getIncome: function () {
+            return values['income'];
+        },
+        getExpense: function () {
+            return values['expense'];
+        },
+        getBalance: function () {
+            return values['balance'];
         }
 
-    })();
 
-    const eventHandlerMod = (function (dispMod, opsMod) {
+    }
 
-        const setupEventHandlers = function () {
-            //Click All Button
-            $('#all-btn').on('click', function () {
-                if (!$('#all-tab').hasClass("output-disp-top")) {
-                    $('#all-tab').addClass("output-disp-top");
-                    $('#income-tab').removeClass("output-disp-top");
-                    $('#expense-tab').removeClass("output-disp-top");
-                }
-            });
+})();
 
-            //Click Expense Button
-            $('#expense-btn').on('click', function () {
-                if (!$('#expense-tab').hasClass("output-disp-top")) {
-                    $('#expense-tab').addClass("output-disp-top");
-                    $('#all-tab').removeClass("output-disp-top");
-                    $('#income-tab').removeClass("output-disp-top");
-                }
-            });
-            //Click Income Button
-            $('#income-btn').on('click', function () {
-                if (!$('#income-tab').hasClass("output-disp-top")) {
-                    $('#income-tab').addClass("output-disp-top");
-                    $('#expense-tab').removeClass("output-disp-top");
-                    $('#all-tab').removeClass("output-disp-top");
-                }
-            });
+//Module for handling user interactions
+const budgetModule = (function (uiMod, opsMod) {
 
-            //Listener for transaction type checkbox
-            $('.uinput-radio-item').on('focus', function() {
-                //Display correct select box
-                $(`.uinput-${$(this).val()}-select`).toggleClass('uinput-invisible');
-                //Hide select box
-                $(`.uinput-${$('.uinput-radio-item:checked').val()}-select`).toggleClass('uinput-invisible');
-            });
+    const setupEventHandlers = function () {
+        //Click All Button
+        $('#all-btn').on('click', function () {
+            if (!$('#all-tab').hasClass("output-disp-top")) {
+                $('#all-tab').addClass("output-disp-top");
+                $('#income-tab').removeClass("output-disp-top");
+                $('#expense-tab').removeClass("output-disp-top");
+            }
+        });
 
-            //Clicking Add Entry button
-            $('#submit').on('click', function(event) {
-                event.preventDefault();
+        //Click Expense Button
+        $('#expense-btn').on('click', function () {
+            if (!$('#expense-tab').hasClass("output-disp-top")) {
+                $('#expense-tab').addClass("output-disp-top");
+                $('#all-tab').removeClass("output-disp-top");
+                $('#income-tab').removeClass("output-disp-top");
+            }
+        });
+        //Click Income Button
+        $('#income-btn').on('click', function () {
+            if (!$('#income-tab').hasClass("output-disp-top")) {
+                $('#income-tab').addClass("output-disp-top");
+                $('#expense-tab').removeClass("output-disp-top");
+                $('#all-tab').removeClass("output-disp-top");
+            }
+        });
 
-                //Store transaction type from checkbox
-                const transactionType = $('.uinput-radio-item:checked').val();
+        //Listener for transaction type checkbox
+        $('.uinput-radio-item').on('focus', function () {
+            //Display correct select box
+            $(`.uinput-${$(this).val()}-select`).toggleClass('uinput-invisible');
+            //Hide select box
+            $(`.uinput-${$('.uinput-radio-item:checked').val()}-select`).toggleClass('uinput-invisible');
+        });
 
-                //Store date
-                const date = $('#date').val(); 
+        //Clicking Add Entry button
+        $('#submit').on('click', function (event) {
+            event.preventDefault();
 
-                //Store category from drop down
-                const category = $(`.uinput-${transactionType}-select option:checked`).val();
+            //Store transaction type from checkbox
+            const transactionType = $('.uinput-radio-item:checked').val();
 
-                //store descriptiom
-                const description = $('#description').val();
+            //Store date
+            const date = $('#date').val();
 
-                //Store amount
-                const amount = $('#amount').val();
+            //Store category from drop down
+            const category = $(`.uinput-${transactionType}-select`).val();
 
-            
+            //store descriptiom
+            const description = $('#description').val();
+
+            //Check if amount is a number
+
+            //Store amount
+            const amount = uiMod.isValidAmount($('#amount').val());
+
+
+            if (amount && date) {
                 //Update Income or Expense and Balance
-                opsMod.updateValues(transactionType,amount);
-
+                opsMod.addValues(transactionType, amount);
+    
                 //Display Income/Expense and Balance
-                dispMod.displayValues(transactionType,opsMod.getExpense(), opsMod.getIncome(), opsMod.getBalance());
-
-
-                
+                uiMod.displayValues(transactionType, opsMod.getExpense(), opsMod.getIncome(), opsMod.getBalance());
+    
+    
+    
                 //display Entry in all table and either Expense or Income
-                dispMod.displayEntry(date, transactionType, category, description, amount);
-                
-                
+                uiMod.displayEntry(date, transactionType, category, description, amount);
+    
+    
                 $('#description').val('');
                 $('#amount').val('');
-
-
-
-            });
-        }
-
-        return {
-            init: function() {
-                setupEventHandlers();
+            } else {
+                //if amount is invalid, display message
+                //if date is empty. display message
             }
+        });
+        //Clicking x to remove entry in all-tab
+        $('#all-tab,#income-tab,#expense-tab').on('click', 'i', function() {
+        
+            //subtract either Income or Expense
+            opsMod.subtractValues(this.id,uiMod.getAllObj());
+            
+            //Remove Entry from Tables
+            uiMod.removeFromTables(this.id);
+
+            //display updated Income, Expense or Balance
+            uiMod.displayValues(this.id, opsMod.getExpense(), opsMod.getIncome(), opsMod.getBalance());
+        });
+
+
+    }
+
+    return {
+        init: function () {
+            setupEventHandlers();
         }
+    }
+})(uiModule, opsModule);
 
-    })(displayMod, operationsMod);
-
-    eventHandlerMod.init();
-
+$(function () {
+    budgetModule.init();
 });
